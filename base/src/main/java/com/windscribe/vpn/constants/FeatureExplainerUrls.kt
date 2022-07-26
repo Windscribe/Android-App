3c452b479cb4ac68c673749237082ecc0a8c8aea
Ginder Singh <ginder@windscribe.com>
2022-07-26 22:07:14 +0000

    Added feature links.

--- base/src/main/java/com/windscribe/vpn/constants/FeatureExplainerUrls.kt
+++ base/src/main/java/com/windscribe/vpn/constants/FeatureExplainerUrls.kt
@@ -1,12 +1,11 @@
 package com.windscribe.vpn.constants
 
 object FeatureExplainer {
-    const val ROBERT = "https://windscribe.com/"
-    const val SPLIT_TUNNELING = "https://windscribe.com/"
-    const val CONNECTION_MODE = "https://windscribe.com/"
-    const val PACKET_SIZE = "https://windscribe.com/"
-    const val KEEP_ALIVE = "https://windscribe.com/"
-    const val ALLOW_LAN = "https://windscribe.com/"
-    const val GPS_SPOOFING = "https://windscribe.com/"
-    const val DECOY_TRAFFIC = "https://windscribe.com/"
+    const val CONNECTION_MODE = "https://windscribe.com/features/flexible-connectivity"
+    const val PACKET_SIZE = "https://windscribe.com/features/packet-size"
+    const val ALLOW_LAN = "https://windscribe.com/features/lan-traffic"
+    const val SPLIT_TUNNELING = "https://windscribe.com/features/split-tunneling"
+    const val ROBERT = "https://windscribe.com/features/robert"
+    const val GPS_SPOOFING = "https://windscribe.com/features/gps-spoofing"
+    const val DECOY_TRAFFIC = "https://windscribe.com/features/decoy-traffic"
 }
