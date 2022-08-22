6735539a8dd5d9bc8e6fb387b5221d17114a7ce8
Ginder Singh <ginder@windscribe.com>
2022-08-22 18:56:31 +0000

    Updated to 3.80

--- base/src/main/java/com/windscribe/vpn/backend/TrafficCounter.kt
+++ base/src/main/java/com/windscribe/vpn/backend/TrafficCounter.kt
@@ -6,6 +6,7 @@ package com.windscribe.vpn.backend
 
 import android.net.TrafficStats
 import com.windscribe.vpn.Windscribe
+import com.windscribe.vpn.Windscribe.Companion.appContext
 import com.windscribe.vpn.apppreference.PreferencesHelper
 import com.windscribe.vpn.commonutils.Ext.launchPeriodicAsync
 import com.windscribe.vpn.commonutils.WindUtilities
@@ -41,7 +42,10 @@ class TrafficCounter(
     init {
         scope.launch {
             vpnConnectionStateManager.state.collect {
-                if (it.status == VPNState.Status.Connected) {
+                if (it.status == VPNState.Status.Connected && appContext.vpnController.vpnBackendHolder.activeBackend?.reconnecting == true) {
+                    logger.debug("VPN reconnecting.")
+                    startUpdatingTrafficStats()
+                } else if (it.status == VPNState.Status.Connected) {
                     resetSession()
                     startUpdatingTrafficStats()
                 } else {
@@ -68,8 +72,8 @@ class TrafficCounter(
                 if (requiresUpdate) {
                     val totalDownload = TrafficStats.getTotalRxBytes()
                     val totalUpload = TrafficStats.getTotalTxBytes()
-                    val downloadDifference = totalDownload - lastDownload
-                    val uploadDifference = totalUpload - lastUpload
+                    val downloadDifference = (totalDownload - lastDownload).coerceAtLeast(0) / 2
+                    val uploadDifference = (totalUpload - lastUpload).coerceAtLeast(0) / 2
                     sessionDownload += downloadDifference
                     sessionUpload += uploadDifference
                     lastDownload = totalDownload
