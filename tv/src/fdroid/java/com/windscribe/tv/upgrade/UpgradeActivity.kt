75c033f1480a55b34253df2c0348fc72b2ba9e4e
Ginder Singh <ginder@windscribe.com>
2023-05-26 22:29:28 -0400

    show error instead of upgrade dialog for fdroid flavour.

--- tv/src/fdroid/java/com/windscribe/tv/upgrade/UpgradeActivity.kt
+++ tv/src/fdroid/java/com/windscribe/tv/upgrade/UpgradeActivity.kt
@@ -8,11 +8,13 @@ import android.content.Intent
 import android.os.Bundle
 import com.windscribe.tv.R
 import com.windscribe.tv.base.BaseActivity
+import com.windscribe.tv.customview.ErrorFragment
 
 class UpgradeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentLayout(R.layout.activity_upgrade)
+        ErrorFragment.instance.add(getString(R.string.billing_unavailable),this, R.id.cl_upgrade, false)
     }
 
     companion object {
