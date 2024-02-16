ccc0f7834a9002e0a7510cfa681def45d92bc30c
Ginder Singh <ginder@windscribe.com>
2024-02-16 21:27:51 +0000

    Updated to 3.80

--- base/src/main/java/com/windscribe/vpn/api/WindscribeDnsResolver.kt
+++ base/src/main/java/com/windscribe/vpn/api/WindscribeDnsResolver.kt
@@ -1,17 +1,21 @@
 package com.windscribe.vpn.api
 
+import android.os.Build
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.windscribe.vpn.Windscribe
+import com.windscribe.vpn.Windscribe.Companion.appContext
 import com.windscribe.vpn.apppreference.PreferencesHelper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import okhttp3.Dns
 import okio.ByteString.Companion.encodeUtf8
+import java.net.Inet4Address
 import java.net.InetAddress
 import java.net.UnknownHostException
 
 class WindscribeDnsResolver(private val mainScope: CoroutineScope, private val preferenceHelper: PreferencesHelper) : Dns {
-    var memoryCache = mutableMapOf<String, String>()
+    private var memoryCache = mutableMapOf<String, String>()
 
     fun addToCache(host: String, ip: String) {
         memoryCache[host] = ip
@@ -21,9 +25,13 @@ class WindscribeDnsResolver(private val mainScope: CoroutineScope, private val p
         val key = hostname.encodeUtf8().md5().hex()
         try {
             if (memoryCache.containsKey(hostname)) {
-                return listOf(InetAddress.getByName(memoryCache[hostname]))
+                return listOf(InetAddress.getByName(memoryCache[hostname])).sortedWith { address1, address2 ->
+                    return@sortedWith sortIpAddresses(hostname, address1, address2)
+                }
             }
-            val addresses = InetAddress.getAllByName(hostname).toList()
+            val addresses = InetAddress.getAllByName(hostname).toList().sortedWith { address1, address2 ->
+                return@sortedWith sortIpAddresses(hostname, address1, address2)
+            }
             cacheDnsResponse(key, addresses)
             return addresses
         } catch (e: UnknownHostException) {
@@ -35,6 +43,22 @@ class WindscribeDnsResolver(private val mainScope: CoroutineScope, private val p
             throw UnknownHostException("Broken system behaviour for dns lookup of $hostname").apply {
                 initCause(e)
             }
+        }
+    }
+
+    private fun preferIpv4(hostname: String): Boolean {
+        val regex = Regex("(assets|api|checkip).[0-9a-f]{40}.com")
+        return appContext.isRegionRestricted && regex.matches(hostname)
+    }
+
+    private fun sortIpAddresses(hostname: String, address1: InetAddress, address2: InetAddress): Int {
+        val isIpv4Address1 = address1 is Inet4Address
+        val isIpv4Address2 = address2 is Inet4Address
+        val preferIpv4 = preferIpv4(hostname)
+        return when {
+            preferIpv4 && isIpv4Address1 && !isIpv4Address2 -> -1
+            preferIpv4 && !isIpv4Address1 && isIpv4Address2 -> 1
+            else -> 0
         }
     }
 
