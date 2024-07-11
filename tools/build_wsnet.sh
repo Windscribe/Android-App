e3bc878821f711dde7410c7e5766a28c34b32c4a
Ginder Singh <ginder@windscribe.com>
2024-07-11 03:33:59 -0400

    remove vcpkg clone instead use srclib

--- tools/build_wsnet.sh
+++ tools/build_wsnet.sh
@@ -4,15 +4,7 @@ DST_DIR="$ROOT_DIR/base/src/main/libs"
 rm -rf tools/bin
 mkdir -p tools/bin
 cd tools/bin || exit
-rm -rf vcpkg
-git clone https://github.com/microsoft/vcpkg.git
-cd vcpkg || exit
-./bootstrap-vcpkg.sh
-export VCPKG_ROOT="$PWD"
 export ANDROID_NDK_HOME="$ANDROID_NDK"
-echo "$ANDROID_NDK_HOME"
-echo "$VCPKG_ROOT"
-cd ..
 git clone https://github.com/Windscribe/Desktop-App.git
 cd Desktop-App || exit
 git checkout 97bfe1dd1b401cd7294f0679b30cede543961b78
