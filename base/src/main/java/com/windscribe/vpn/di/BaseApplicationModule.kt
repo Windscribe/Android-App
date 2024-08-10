11e7742002222194ac3f7893456418a2aba2baaa
Ginder Singh <ginder@windscribe.com>
2024-08-10 17:36:35 -0400

    build device uid from username.

--- base/src/main/java/com/windscribe/vpn/di/BaseApplicationModule.kt
+++ base/src/main/java/com/windscribe/vpn/di/BaseApplicationModule.kt
@@ -779,8 +779,7 @@ open class BaseApplicationModule {
             val msg = it.split(Regex("\\]\\s*")).lastOrNull()?.trim() ?: ""
             logger.debug(msg)
         }, BuildConfig.DEV)
-        WSNet.initialize("android", "android", WindUtilities.getVersionName(),
-            preferencesHelper.sessionHash?.let { preferencesHelper.getDeviceUUID(it) } ?: "", "2.6.0",  BuildConfig.DEV, preferencesHelper.wsNetSettings)
+        WSNet.initialize("android", "android", WindUtilities.getVersionName(), preferencesHelper.getDeviceUUID(preferencesHelper.userName), "2.6.0",  BuildConfig.DEV, preferencesHelper.wsNetSettings)
         val networkListener = object : DeviceStateManager.DeviceStateListener {
             override fun onNetworkStateChanged() {
                 super.onNetworkStateChanged()
