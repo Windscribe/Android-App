f9f8c0cfe79b93905a65fea9cfb6d97578d8a90c
Ginder Singh <ginder@windscribe.com>
2025-02-12 23:15:50 -0500

    Revert "updated wsnet."
    This reverts commit 2b7bff7f314f45355c595670e87fcab1ab30178b.

--- tools/build_wsnet.sh
+++ tools/build_wsnet.sh
@@ -7,7 +7,7 @@ cd tools/bin || exit
 export ANDROID_NDK_HOME="$ANDROID_NDK"
 git clone https://github.com/Windscribe/Desktop-App.git
 cd Desktop-App || exit
-git checkout b477b15e947a3fbef76bf8e957fbea453ecd2a27
+git checkout e359d8d2a634f4080ee7f7fecc0511f38ba8d35c
 cd libs/wsnet/tools || exit
 ./build_android.sh
 cp wsnet.aar "$DST_DIR"
