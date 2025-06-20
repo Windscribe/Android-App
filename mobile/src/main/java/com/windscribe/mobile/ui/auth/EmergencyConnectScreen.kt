3ea40d297afd4f5d979c279b4f9020b4777e8f34
Ginder Singh <ginder@windscribe.com>
2025-06-20 12:21:08 -0400

    removed unused styles

--- mobile/src/main/java/com/windscribe/mobile/ui/auth/EmergencyConnectScreen.kt
+++ mobile/src/main/java/com/windscribe/mobile/ui/auth/EmergencyConnectScreen.kt
@@ -1,7 +1,9 @@
 package com.windscribe.mobile.ui.auth
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -32,6 +34,7 @@ import com.windscribe.mobile.ui.common.AppBackground
 import com.windscribe.mobile.ui.common.NextButton
 import com.windscribe.mobile.ui.helper.MultiDevicePreview
 import com.windscribe.mobile.ui.helper.PreviewWithNav
+import com.windscribe.mobile.ui.home.HandleToast
 import com.windscribe.mobile.ui.nav.LocalNavController
 import com.windscribe.mobile.ui.theme.AppColors
 import com.windscribe.mobile.ui.theme.font16
