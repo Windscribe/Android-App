5520adce01a1ab38240d8a1f253a1aca40bcf3eb
Ginder Singh <ginder@windscribe.com>
2025-07-03 13:03:13 -0400

    fix token generation.

--- base/src/google/java/com.windscribe.vpn/services/firebasecloud/FireBaseManagerImpl.kt
+++ base/src/google/java/com.windscribe.vpn/services/firebasecloud/FireBaseManagerImpl.kt
@@ -34,7 +34,6 @@ class FireBaseManagerImpl(private val context: Windscribe): FirebaseManager {
                 .setApplicationId(BuildConfig.APP_ID)
                 .setProjectId(BuildConfig.PROJECT_ID)
                 .setApiKey(BuildConfig.API_KEY)
-                .setApplicationId("com.windscribe.vpn")
                 .build())
     }
 
