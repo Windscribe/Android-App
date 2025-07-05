00563d7e5ff9ea07fd5d68d7a55edb06b13e850b
Ginder Singh <ginder@windscribe.com>
2025-07-05 12:08:23 -0400

    use gradlew.

--- tools/build_strongswan.sh
+++ tools/build_strongswan.sh
@@ -51,7 +51,7 @@ cd "$APP_DIR"
 sed -i '/arguments '\''-j'\'' \+ Runtime\.runtime\.availableProcessors()/a\        arguments "-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON"' build.gradle
 
 echo "===> Building release APK..."
-./../.gradlew assembleRelease
+gradle assembleRelease
 
 echo "===> Copying output libs to: $STRONGSWAN_LIBS_DST"
 mkdir -p "$STRONGSWAN_LIBS_DST"
