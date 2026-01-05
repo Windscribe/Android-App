74a5159eb0b8ecbf0359fd6a8a04e7a07eb9d13c
Ginder Singh <ginder@windscribe.com>
2026-01-05 13:46:57 -0500

    update wsnet.

--- tools/build_wsnet.sh
+++ tools/build_wsnet.sh
@@ -7,8 +7,8 @@ cd tools/bin || exit
 export ANDROID_NDK_HOME="$ANDROID_NDK"
 git clone https://github.com/Windscribe/Desktop-App.git
 cd Desktop-App || exit
-## WSNet 2.18.8
-git checkout 7a7cf5f073defb7002a71f1032052b9e2f2f5939
+## WSNet 2.19.7
+git checkout 756dabf7cdd86f13c9840511c01a61ca3153eedc
 cd libs/wsnet/tools || exit
 ./build_android.sh > /dev/null 2>&1
 cp wsnet.aar "$DST_DIR"
