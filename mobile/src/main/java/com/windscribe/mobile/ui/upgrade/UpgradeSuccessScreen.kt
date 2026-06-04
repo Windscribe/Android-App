774b566e961dc8f8ecf3be95549a1ca7704e5153
Ginder Singh <ginder@windscribe.com>
2026-06-04 11:26:09 -0400

    lint warning fix.

--- mobile/src/main/java/com/windscribe/mobile/ui/upgrade/UpgradeSuccessScreen.kt
+++ mobile/src/main/java/com/windscribe/mobile/ui/upgrade/UpgradeSuccessScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import com.windscribe.mobile.R
